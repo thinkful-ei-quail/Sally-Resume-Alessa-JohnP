361293ab5461a1a6bb7b267547d45b44d19c729c
John Pendergast <jpending@gmail.com>
2020-07-20 11:58:37 -0400

    got the react app to render the information

--- sally-resume-app/src/App.js
+++ sally-resume-app/src/App.js
@@ -3,24 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div>
+<header>
+    <div id="img">
+        <img src="./ilana.jpg" alt="This is not Sally." />
     </div>
+    <h1 class="title">About me</h1>
+</header>
+
+<main>
+    <section id="introduction">
+        <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
+    </section>
+    <br />
+    <section id="Education">
+        <p>I have studied at:</p>
+        <ul>
+            <li>IES BEZMILIANA</li>
+            <li>Minnesota University</li>
+        </ul>
+    </section>
+    <br />
+    <section id="works">
+        <p>I have worked at:</p>
+        <div id="works_list">
+            <ul>
+                <li>Github as Engineer during 2018 and there I have to
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </li>
+                <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
+                    A assumenda autem dolorem doloremque
+                </li>
+            </ul>
+        </div>
+    </section>
+    <br />
+    <section id="Contact-section">
+        <p>My contact information: </p>
+        <address id="Contact-info">
+            <p>My name is Sally</p>
+            <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
+            <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
+        </address>
+    </section>
+    </main>
+  </div>
   );
+
 }
+
+
+
 
 export default App;
